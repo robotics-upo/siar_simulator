207c40f758a17221399c9e16584b345f3030e652
saimon <simon.martinez.rzs@gmail.com>
2019-11-06 14:56:36 +0100

    Adding simple world to testing SIAR

---
+++ scripts/installation_siar_simulator.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Installation GAZEBO
+
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gazebo7
+sudo apt-get install libgazebo7-dev
+
+
+#Installing ROS dependencies 
+
+sudo apt-get install ros-$ROS_DISTRO-gazebo-ros-pkgs ros-$ROS_DISTRO-gazebo-ros-control
+
+sudo apt-get install ros-$ROS_DISTRO-velodyne-gazebo-plugins ros-$ROS_DISTRO-twist-mux ros-$ROS_DISTRO-joy
+
+mkdir -p ~/siar_ws/src
+cd ~/siar_ws/src
+git clone https://github.com/robotics-upo/siar_packages.git 
+git clone https://github.com/robotics-upo/functions.git
+
+cd ~/siar_ws
+catkin_make --pkg siar_arm
+catkin_make --pkg siar_driver
+catkin_make
--- siar_gazebo/scripts/install_siar_simulator.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install ros-kinetic-joint-state-controller
-sudo apt-get install ros-kinetic-effort-controllers
-sudo apt-get install ros-kinetic-position-controllers
-
-
-
